280c7514187bf97cf561d0d5b277b99ac1d172ae
Rohan <rohanmolinillo@gmail.com>
2022-11-07 11:57:03 +0100

    Creat hello app

---
+++ index.js
@@ -0,0 +1,4 @@
+require('dotenv').config();
+
+// eslint-disable-next-line no-console
+console.log(`I am ${process.env.MY_NAME}, wilder in ${process.env.MY_CITY}, and I love ${process.env.MY_LANGUAGE}`);
